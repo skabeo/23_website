00ff50aeeb4b261481a9709270f881c4036cefd9
skabeo <spencerokyere95@gmail.com>
2024-10-18 08:43:48 +0000

    Edit title

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ import Footer from '@/components/Footer';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Prime Properties - Real Estate Agency',
+  title: '23 Properties - Real Estate Agency',
   description: 'Your trusted partner in finding your dream home',
 };
 
